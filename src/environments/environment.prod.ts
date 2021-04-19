9746df003200d786960ae257661bcfa8ad1b7bc0
thiagoassis2 <52789439+thiagoassis2@users.noreply.github.com>
2021-04-19 10:56:50 -0300

    formulário

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyB8x0Q85WCZxhngzlBZ4ej4b_5hiG3K-Ig",
-    authDomain: "projeto5-d8a7c.firebaseapp.com",
-    projectId: "projeto5-d8a7c",
-    storageBucket: "projeto5-d8a7c.appspot.com",
-    messagingSenderId: "767611244675",
-    appId: "1:767611244675:web:f5e55cf2ca0ce80cc48bb6"
+    apiKey: "AIzaSyBkMuNJ-OLg4jQqLMVBjUiOiuQOIEO31fM",
+    authDomain: "projeto5-4c455.firebaseapp.com",
+    projectId: "projeto5-4c455",
+    storageBucket: "projeto5-4c455.appspot.com",
+    messagingSenderId: "703367568946",
+    appId: "1:703367568946:web:a52180d1c89efc6adae62b"
   }
 };
